ca98db2eedc23eaa6c00c811e90a2c0a820c6a90
fsaiz97 <felixsaiz97@gmail.com>
2022-11-19 17:44:40 +0000

    Fix previous merge PageWrapper.jsx - Remove extra InnerNav import

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -3,8 +3,6 @@ import { Outlet, useNavigate } from 'react-router-dom';
 
 import InnerNav from '../InnerNav';
 
-import InnerNav from '../InnerNav';
-
 function PageWrapper({ motto, navSearchSearching, setMotto }) {
   const [isLoading, setIsLoading] = useState(true);
   const [boroughData, setBoroughData] = useState([]);
